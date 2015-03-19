3068d5929f7b13d9cf92fdb1ee62b48aae6d0325
Jerry C. <jerry@mainstreetdata.net>
2015-03-19 13:07:32 -0500

    Update clear_cache.rb

--- symfony/recipes/clear_cache.rb
+++ symfony/recipes/clear_cache.rb
@@ -5,13 +5,13 @@ node[:deploy].each do |application, deploy|
   #execute 'clear_symfony_cache_prod' do
   script "clear_symfony_cache" do
     interpreter "bash"
-    user "root"
+    user "www-data"
     cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    php app/console cache:clear --env=prod --no-debug 
-    EOH
+    #code <<-EOH
+    #php app/console cache:clear --env=prod --no-debug 
+    #EOH
     # The command below doesn't appear to be running..so added the above block
-    command "php app/console cache:clear --env=prod --no-debug"
+    command "php app/console cache:clear --no-debug"
     action :run
   end
 end
